92d851029824972d9dda26a469a6a356d87d40fa
Rade Stojanovic <stojanovicrade614@gmail.com>
2023-03-25 12:26:36 +0100

    model

--- api/model/flight.go
+++ api/model/flight.go
@@ -8,8 +8,14 @@ import (
 )
 
 type Flight struct {
-	ID          primitive.ObjectID `bson:"_id,omitempty" json:"id"`
-	TicketPrice float32            `bson:"ticketPrice,omitempty" json:"ticketPrice"`
+	ID              primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	TicketPrice     float32            `bson:"ticketPrice,omitempty" json:"ticketPrice"`
+	DateSource      primitive.DateTime `bson:"dateSource,omitempty" json:"dateSource"`
+	DateDestination primitive.DateTime `bson:"dateDestination,omitempty" json:"dateDestination"`
+	Pid             string             `bson:"pid,omitempty" json:"pid"`
+	DateOfBirth     primitive.DateTime `bson:"dateOfBirth,omitempty" json:"dateOfBirth"`
+	PhoneNumber     string             `bson:"phoneNumber,omitempty" json:"phoneNumber"`
+	Nationality     string             `bson:"nationality,omitempty" json:"nationality"`
 }
 
 type Flights []*Flight
