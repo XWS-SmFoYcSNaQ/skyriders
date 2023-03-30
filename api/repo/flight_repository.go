2fa4c21da6bcc164464db86523e31ff16f29da54
Rade Stojanovic <stojanovicrade614@gmail.com>
2023-03-30 22:32:49 +0200

    basic filters

--- api/repo/flight_repository.go
+++ api/repo/flight_repository.go
@@ -2,11 +2,13 @@ package repo
 
 import (
 	"Skyriders/model"
+	"Skyriders/utils"
 	"context"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"log"
 	"time"
 
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -19,12 +21,11 @@ func CreateFlightRepo(l *log.Logger, c *mongo.Collection) *FlightRepo {
 	return &FlightRepo{l, c}
 }
 
-func (fr *FlightRepo) GetAll() (model.Flights, error) {
+func (fr *FlightRepo) GetAll(filters map[string][]string) (model.Flights, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-
 	var flights model.Flights
-	flightsCursor, err := fr.db.Find(ctx, bson.M{})
+	flightsCursor, err := fr.db.Find(ctx, utils.ConvertFlightFilterData(filters))
 	if err != nil {
 		fr.logger.Println(err)
 		return nil, err
@@ -49,10 +50,10 @@ func (fr *FlightRepo) Create(flight *model.Flight) error {
 	return nil
 }
 
-func (fr *FlightRepo) Delete(flight *model.Flight) error {
+func (fr *FlightRepo) Delete(id primitive.ObjectID) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-	_, err := fr.db.DeleteOne(ctx, &flight)
+	_, err := fr.db.DeleteOne(ctx, bson.M{"_id": id})
 	if err != nil {
 		fr.logger.Println(err)
 		return err
